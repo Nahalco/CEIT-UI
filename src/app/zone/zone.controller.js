baaa0d305359415f4ee8a930f9be7fc09de912a6
majidln <majidlotfinia12@yahoo.com>
2016-11-14 10:31:19 +0330

    drag drop template be ok

--- src/app/zone/zone.controller.js
+++ src/app/zone/zone.controller.js
@@ -9,8 +9,8 @@
   function ZoneController($scope) {
     var vm = this;
 
-    vm.lamps = [{"id" : "2:3",'title': 'l1'},{"id" : "2:2",'title': 'l2'},{"id" : "1:3",'title': 'l3'}];
-    vm.multisensors = [{"id" : "2:3",'title': 'l1'},{"id" : "2:2",'title': 'l2'},{"id" : "1:3",'title': 'l3'}];
+    vm.lamps = [{"id" : "2:3",'title': 'l1', "type" : "lamp"},{"id" : "2:2",'title': 'l2', "type" : "lamp"},{"id" : "1:3",'title': 'l3', "type" : "lamp"}];
+    vm.multisensors = [{"id" : "2:3",'title': 'm1', "type" : "multi"},{"id" : "2:2",'title': 'm2', "type" : "multi"},{"id" : "1:3",'title': 'm3', "type" : "multi"}];
     vm.coolers = [{"id" : "2:3",'title': 'l1'},{"id" : "2:2",'title': 'l2'},{"id" : "1:3",'title': 'l3'}];
 
     $scope.list1 = [{'title': 'l1'},{'title': 'l2'},{'title': 'l3'}];
@@ -24,6 +24,25 @@
       }
     }
 
+    vm.dragCallback = function () {
+            console.log("dragCallback");
+        };
+        vm.dropCallback = function () {
+            console.log("dropCallback");
+        };
+        vm.overCallback = function () {
+            console.log("overCallback");
+        };
+        $scope.startCallback = function (event, ui) {
+        	  console.log("startCallback" );
+        };
+        $scope.stopCallback = function (event) {
+          console.log("stopCallback" )
 
+        }
+        $scope.onDrop = function (event) {
+          console.log("onDrop" )
+        }
+        //kalaghsefid 10
   }
 })();
