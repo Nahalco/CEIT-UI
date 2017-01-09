6ea38b66812ccaae3a10953174905999daa2249f
saman <samanf74@gmail.com>
2017-01-09 20:28:36 +0330

    states added to plugin

--- src/app/plugin/plugin.controller.js
+++ src/app/plugin/plugin.controller.js
@@ -8,7 +8,10 @@
   /** @ngInject */
   function PluginController($scope , $http, Table,$sce) {
     var vm = this;
-    vm.zones = [];
+
+    $scope.states = [];
+    $scope.stateTypes = ["filter", "notify"];
+    /*vm.zones = [];
 
 
     vm.list1 = [];
@@ -17,9 +20,23 @@
     $scope.list5 = [
       { 'title': 'Filter', 'drag': true , 'type':"filter" },
       { 'title': 'Notify', 'drag': true , 'type':"notify" },
-    ];
+    ];*/
+    //var vm = this;
 
-
-
+    $scope.addState = function(){
+      var x = {}
+      $scope.states.push(x)
+    }
+    $scope.stateAccept = function(state,state_name,state_agent_id,state_type){
+      for(var key in state){
+        if(key != state_name){
+          delete state[key];
+        }
+      }
+      state[state_name] = {agent_id:state_agent_id,type:state_type,conditions:[]}
+      //console.log(state)
+      //console.log('++++++++')
+      console.log($scope.states)
+    }
   }
 })();
