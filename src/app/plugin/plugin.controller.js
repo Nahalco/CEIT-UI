b5509e28334ff2a1a9b3de378bdcc098c40301fd
saman <samanf74@gmail.com>
2017-01-10 16:27:42 +0330

    notify and sub of it included

--- src/app/plugin/plugin.controller.js
+++ src/app/plugin/plugin.controller.js
@@ -11,6 +11,10 @@
 
     $scope.states = [];
     $scope.stateTypes = ["filter", "notif"];
+    $scope.notifSettings=[
+      {"item":"on","values":["true","false"]},
+      {"item":"off","values":["true","false"]},
+    ]
     /*vm.zones = [];
 
 
@@ -25,7 +29,7 @@
 
 
     $scope.addState = function(){
-      var txt = '{"name":"","agent_id":"","type":"","conditions":[],"actions":[]}'
+      var txt = '{"name":"","agent_id":"","type":"","conditions":[],"actions":[], "nextStep":""}'
       var x = JSON.parse(txt)
       $scope.states.push(x)
       //console.log( $scope.states)
@@ -52,6 +56,12 @@
       state['conditions'] = deleteIndex(state['conditions'],state['conditions'].indexOf(cond))
       //console.log(state['conditions'])
     }
+
+    $scope.addAction = function(state){
+      var txt = '{"type":"","agent_id":"","device_id":[],"settings":{}}'
+      var x = JSON.parse(txt)
+      state['actions'].push(x)
+    }
     /*$scope.stateAccept = function(state){
       console.log($scope.states)
       //state = {name:state_name,agent_id:state_agent_id,type:state_type,conditions:[],stat:1}
@@ -66,6 +76,33 @@
       //console.log($scope.states)
       $scope.states = deleteIndex($scope.states,$scope.states.indexOf(state))
       //console.log($scope.states)
+    }
+
+    $scope.fillSettings = function(act, name){
+      for(var key in act['settings']){
+        if(key != name){
+          delete act['settings'][key];
+        }
+      }
+    }
+
+    $scope.deleteAct = function(state,act){
+      //console.log(cond)
+      state['actions'] = deleteIndex(state['actions'],state['actions'].indexOf(act))
+      //console.log(state['conditions'])
+    }
+
+    $scope.addDevice = function(act){
+      var txt = ''
+      if(!act['device_id'].includes('')) {
+        act['device_id'].push(txt)
+      }
+    }
+
+    $scope.deleteDevice = function(act,device){
+      //console.log(cond)
+      act['device_id'] = deleteIndex(act['device_id'],act['device_id'].indexOf(device))
+      //console.log(state['conditions'])
     }
   }
 })();
