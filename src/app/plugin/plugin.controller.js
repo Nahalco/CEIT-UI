77c8290f54336a37ab31f7510d86aaa2edcd66cb
saman <samanf74@gmail.com>
2017-01-10 13:44:23 +0330

    state and conditions added

--- src/app/plugin/plugin.controller.js
+++ src/app/plugin/plugin.controller.js
@@ -23,20 +23,34 @@
     ];*/
     //var vm = this;
 
-    $scope.addCond = function(state){
-      var txt = '{"type":"","device_id":"","states":[],"actions":""}'
-      var x = JSON.parse(txt)
-      for(var key in state) {
-        if(state[key].conditions != undefined){
-          state[key].conditions.push(x)
-        }
-      }
-    }
+
     $scope.addState = function(){
-      var txt = '{"name":"","agent_id":"","type":"","conditions":[],"stat":"0"}'
+      var txt = '{"name":"","agent_id":"","type":"","conditions":[],"actions":[]}'
       var x = JSON.parse(txt)
       $scope.states.push(x)
       //console.log( $scope.states)
+    }
+    $scope.addCond = function(state){
+      //console.log(state)
+      var txt = '{"type":"","device_id":"","states":[],"action":{"nextStep":""}}'
+      var x = JSON.parse(txt)
+      state['conditions'].push(x)
+    }
+    $scope.addSt = function(cond){
+      //console.log(cond)
+      var txt = '{"type":"","value":"","op":"","logic":""}'
+      var x = JSON.parse(txt)
+      cond['states'].push(x)
+    }
+    $scope.deleteSt = function(cond,st){
+      //console.log(cond)
+      cond['states'] = deleteIndex(cond['states'],cond['states'].indexOf(st))
+      //console.log(state['conditions'])
+    }
+    $scope.deleteCond = function(state,cond){
+      //console.log(cond)
+      state['conditions'] = deleteIndex(state['conditions'],state['conditions'].indexOf(cond))
+      //console.log(state['conditions'])
     }
     /*$scope.stateAccept = function(state){
       console.log($scope.states)
@@ -45,10 +59,20 @@
       console.log('++++++++')
       console.log($scope.states)
     }*/
-    $scope.prevClick = function(state){
+    $scope.check = function(state){
       console.log(state)
-      state['stat'] = 0
-      console.log(state)
+    }
+    $scope.deleteState = function(state){
+      //console.log($scope.states)
+      $scope.states = deleteIndex($scope.states,$scope.states.indexOf(state))
+      //console.log($scope.states)
     }
   }
 })();
+
+function deleteIndex(array,index){
+  if(index == 0){
+    return array.splice(1,array.length)
+  }
+  return (array.splice(0,index).concat(array.splice(index,array.length)))
+}
