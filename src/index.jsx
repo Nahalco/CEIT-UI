9d16ab434c30c7c69c833394843d0d640e979cf1
Parham Alvani <parham.alvani@gmail.com>
2017-06-19 04:01:37 +0430

    Application entry point was added.

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+// Application entrypoint.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<App />, document.getElementById('react-root'));
