0439faa43ded0f7027c6e5e73a7a1d192be6691c
Parham Alvani <parham.alvani@gmail.com>
2017-06-19 08:10:47 +0430

    Improves app layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Multisensor from './Multisensor.jsx';
+import Mode from './Mode.jsx';
 
 
 class App extends React.Component {
@@ -9,28 +10,31 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="row">
+      <div className="col">
         <div className="row">
-          <h1 className="display-2">
-            Meeting Room 
-            <small className="text-muted h4">{new Date().getFullYear()}</small>
-          </h1>
+          <p className="col text-center">
+            <h1 className="display-2">
+              Meeting Room 
+              <small className="text-muted h4">{new Date().getFullYear()}</small>
+            </h1>
+          </p>
         </div>
-      <div className="row">
-        <div className="col-1">
-          <img className="row img-thumbnail border-0" src="img/aut.png" alt="AUT Logo" />
-          <img className="row img-thumbnail border-0" src="img/ceit.png" alt="CEIT Logo" />
-          <img className="row img-thumbnail border-0" src="img/aolab.png" alt="Aolab Logo" />
-        </div>
-        <div className="col-8">
-          <div>
+        <div className="row">
+          <div className="col-2">
+            <img className="row img-thumbnail border-0" src="img/aut.png" alt="AUT Logo" />
+            <img className="row img-thumbnail border-0" src="img/ceit.png" alt="CEIT Logo" />
+            <img className="row img-thumbnail border-0" src="img/aolab.png" alt="Aolab Logo" />
+          </div>
+          <div className="col-8">
+            <div className="card-group">
+              <Mode title="Presentation" />
+            </div>
+          </div>
+          <div className="col-2">
+            <Multisensor />
           </div>
         </div>
-        <div className="col">
-          <Multisensor />
-        </div>
       </div>
-    </div>
     );
   }
 }
