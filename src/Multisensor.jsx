b1d79613d26b1fda5e15254c5fa4681099111ccf
Parham Alvani <parham.alvani@gmail.com>
2017-06-21 11:41:20 +0430

    Multisensor component was improved.

--- src/Multisensor.jsx
+++ src/Multisensor.jsx
@@ -1,47 +1,35 @@
 import React from 'react';
+import Gauge from 'react-gauge';
 
 
 class Multisensor extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.thing = props.thing;
+    this.state = {
+      temperature: 0,
+      humidity: 0,
+      light: 0
+    }
+  }
+
+  componentDidMount() {
+    this.thing.log.then((result) => {
+      this.setState({
+        temperature: result['temperature']['value'],
+        humidity: result['humidity']['value'],
+        light: result['light']['value']
+      });
+    });
   }
 
   render() {
     return (
       <div>
-        <canvas data-type="radial-gauge"
-          data-width="300"
-          data-height="300"
-          data-title="Temperature"
-          data-min-value="0"
-          data-max-value="50"
-          data-major-ticks="0,5,10,15,20,25,30,35,40,45,50"
-          data-minor-ticks="2"
-          data-value="22.3"
-          data-units="°C"
-        ></canvas>
-        <canvas data-type="radial-gauge"
-          data-width="300"
-          data-height="300"
-          data-title="Humidity"
-          data-min-value="0"
-          data-max-value="100"
-          data-major-ticks="0,10,20,30,40,50,60,70,80,90,100"
-          data-minor-ticks="2"
-          data-value="50"
-          data-units="%"
-        ></canvas>
-        <canvas data-type="radial-gauge"
-          data-width="300"
-          data-height="300"
-          data-title="Light"
-          data-min-value="0"
-          data-max-value="50"
-          data-major-ticks="0,10,20,30,40,50"
-          data-minor-ticks="5"
-          data-value="22.3"
-          data-units="false"
-        ></canvas>
+        <h6>{this.thing.id} @ {this.thing.agentId}</h6>
+        <Gauge value={this.state.temperature}/>
+        <Gauge value={this.state.humidity}/>
+        <Gauge value={this.state.light / 10}/>
       </div>
     );
   }
