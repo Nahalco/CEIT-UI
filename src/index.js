f5d21503a0c994bbf5e02b1e4ef09fed07327f0e
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 01:13:48 +0430

    index.js reformatted.

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@
  * | File Name:     index.js
  * +===============================================
  */
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Dashboard from './components/Dashboard';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import Dashboard from './components/Dashboard'
 
-ReactDOM.render(<Dashboard />,  document.getElementById('root'));
+ReactDOM.render(<Dashboard />, document.getElementById('root'))
