237258299713a4ade703b05a9b1f1a27b906770f
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 00:48:57 +0430

    Webpack configuration style was improved.

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const proxy = require('./server/webpack-dev-proxy');
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const proxy = require('./server/webpack-dev-proxy')
 
-function getEntrySources(sources) {
+function getEntrySources (sources) {
   if (process.env.NODE_ENV !== 'production') {
-    sources.push('webpack-hot-middleware/client');
+    sources.push('webpack-hot-middleware/client')
   }
 
-  return sources;
+  return sources
 }
 
 const basePlugins = [
   new webpack.DefinePlugin({
     __DEV__: process.env.NODE_ENV !== 'production',
     __PRODUCTION__: process.env.NODE_ENV === 'production',
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
   }),
   new HtmlWebpackPlugin({
     template: './src/index.html',
-    inject: 'body',
-  }),
-];
+    inject: 'body'
+  })
+]
 
 const devPlugins = [
-  new webpack.NoErrorsPlugin(),
-];
+  new webpack.NoErrorsPlugin()
+]
 
 const prodPlugins = [
   new webpack.optimize.OccurenceOrderPlugin(),
   new webpack.optimize.UglifyJsPlugin({
     compressor: {
-      warnings: false,
-    },
-  }),
-];
+      warnings: false
+    }
+  })
+]
 
 const plugins = basePlugins
   .concat(process.env.NODE_ENV === 'production' ? prodPlugins : [])
-  .concat(process.env.NODE_ENV === 'development' ? devPlugins : []);
+  .concat(process.env.NODE_ENV === 'development' ? devPlugins : [])
 
 module.exports = {
   entry: {
@@ -47,38 +47,38 @@ module.exports = {
       'es5-shim',
       'es6-shim',
       'es6-promise',
-      'react',
-    ],
+      'react'
+    ]
   },
 
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].[hash].js',
     publicPath: '/',
-    chunkFilename: '[id].chunk.js',
+    chunkFilename: '[id].chunk.js'
   },
   plugins: plugins,
 
   devServer: {
     historyApiFallback: { index: '/' },
-    proxy: proxy(),
+    proxy: proxy()
   },
 
   module: {
     preLoaders: [
       { test: /\.js$/, loader: 'source-map-loader' },
-      { test: /\.js$/, loader: 'eslint-loader' },
+      { test: /\.js$/, loader: 'eslint-loader' }
     ],
     loaders: [
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.(js|jsx)$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
       { test: /\.json$/, loader: 'json-loader' },
       { test: /\.(png|jpg|jpeg|gif|svg)$/, loader: 'url-loader?prefix=img/&limit=5000' },
-      { test: /\.(woff|woff2|ttf|eot)$/, loader: 'url-loader?prefix=font/&limit=5000' },
-    ],
+      { test: /\.(woff|woff2|ttf|eot)$/, loader: 'url-loader?prefix=font/&limit=5000' }
+    ]
   },
 
   resolve: {
-    extensions: ['', '.js', '.jsx'],
-  },
-};
+    extensions: ['', '.js', '.jsx']
+  }
+}
