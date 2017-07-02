e72caf00193f1ba77532e06f11dcbdc1a3f86b1b
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 02:23:08 +0430

    Miltisensor widget was improved.

--- src/components/widgets/Multisensor.jsx
+++ src/components/widgets/Multisensor.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from 'react'
 
-import {RadialGauge} from 'react-canvas-gauges';
+import {RadialGauge} from 'react-canvas-gauges'
+import {I1820Thing} from '@i1820/api'
 
 class Multisensor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.thing = props.thing;
+  constructor (props) {
+    super(props)
+
+    this.thing = props.thing
+
     this.state = {
       temperature: 0,
       humidity: 0,
       light: 0
     }
+
     this.thing.on('log', (result) => {
       console.log(result)
       this.setState({
@@ -21,17 +25,17 @@ class Multisensor extends React.Component {
     })
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.thing.log.then((result) => {
       this.setState({
         temperature: result['temperature']['value'],
         humidity: result['humidity']['value'],
         light: result['light']['value']
-      });
-    });
+      })
+    })
   }
 
-  render() {
+  render () {
     return (
       <div>
         <RadialGauge
@@ -42,8 +46,7 @@ class Multisensor extends React.Component {
           minValue={0}
           maxValue={50}
           majorTicks={['0', '5', '15', '20', '25', '30', '35', '40', '45', '50']}
-          minorTicks={2}
-        ></RadialGauge>
+          minorTicks={2} />
         <RadialGauge
           units='%'
           title='Humidity'
@@ -52,8 +55,7 @@ class Multisensor extends React.Component {
           minValue={0}
           maxValue={100}
           majorTicks={['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']}
-          minorTicks={5}
-        ></RadialGauge>
+          minorTicks={5} />
         <RadialGauge
           units='%'
           title='Light'
@@ -62,11 +64,14 @@ class Multisensor extends React.Component {
           minValue={0}
           maxValue={100}
           majorTicks={['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']}
-          minorTicks={5}
-        ></RadialGauge>
+          minorTicks={5} />
       </div>
-    );
+    )
   }
 }
 
-export default Multisensor;
+Multisensor.propTypes = {
+  thing: React.PropTypes.instanceOf(I1820Thing)
+}
+
+export default Multisensor
