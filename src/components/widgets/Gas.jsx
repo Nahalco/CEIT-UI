31578f52d1e5a24e9e6d68e79bcb5df99b34087a
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 03:28:01 +0430

    Gas widget was improved.

--- src/components/widgets/Gas.jsx
+++ src/components/widgets/Gas.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import {RadialGauge} from 'react-canvas-gauges'
+import {I1820Thing} from '@i1820/api'
 
 class Gas extends React.Component {
   constructor (props) {
@@ -25,14 +26,21 @@ class Gas extends React.Component {
         <RadialGauge
           title='Gas'
           height={300}
-          value={this.state.gas - 512}
+          value={this.state.gas / 10}
           minValue={0}
-          maxValue={200}
+          maxValue={100}
           majorTicks={['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']}
+          highlights={[
+            { from: 65, to: 100, color: 'rgba(255,0,0,.15)' }
+          ]}
           minorTicks={2} />
       </div>
     )
   }
+}
+
+Gas.propTypes = {
+  thing: React.PropTypes.instanceOf(I1820Thing)
 }
 
 export default Gas
