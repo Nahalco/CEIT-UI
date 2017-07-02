d325d31b322ff8a73bdb0b75c5d1135dcd3d722a
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 01:18:23 +0430

    Reformatting.

--- server/node-server.js
+++ server/node-server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const winston = require('winston');
-const helmet = require('helmet');
-const nodeProxy = require('./node-proxy');
-const nodeAppServer = require('./node-app-server');
+const express = require('express')
+const winston = require('winston')
+const helmet = require('helmet')
+const nodeProxy = require('./node-proxy')
+const nodeAppServer = require('./node-app-server')
 
 /**
  * Heroku-friendly production http server.
@@ -10,25 +10,25 @@ const nodeAppServer = require('./node-app-server');
  * Serves your app and allows you to proxy APIs if needed.
  */
 
-const app = express();
-const PORT = process.env.PORT || 8080;
+const app = express()
+const PORT = process.env.PORT || 8080
 
 // Enable various security helpers.
-app.use(helmet());
+app.use(helmet())
 
 // API proxy logic: if you need to talk to a remote server from your client-side
 // app you can proxy it though here by editing ./proxy-config.js
-nodeProxy(app);
+nodeProxy(app)
 
 // Serve the distributed assets and allow HTML5 mode routing. NB: must be last.
-nodeAppServer(app);
+nodeAppServer(app)
 
 // Start up the server.
 app.listen(PORT, (err) => {
   if (err) {
-    winston.error(err);
-    return;
+    winston.error(err)
+    return
   }
 
-  winston.info(`Listening on port ${PORT}`);
-});
+  winston.info(`Listening on port ${PORT}`)
+})
