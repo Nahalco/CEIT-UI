a7c65d56d729ee86fd1bf52fde3667c43dd90692
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 00:26:49 +0430

    Dashboard settings were improved.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,15 +3,11 @@ import Dashboard, { addWidget } from 'react-dazzle';
 
 // App components
 import Header from './Header';
-import EditBar from './EditBar';
 import Container from './Container';
-import AddWidgetDialog from './AddWidgetDialog';
 import CustomFrame from './CustomFrame';
 
 // Widgets of the dashboard.
-import BarChart from './widgets/BarChart';
-import LineChart from './widgets/LineChart';
-import DoughnutChart from './widgets/DoughnutChart';
+import Multisensor from './widgets/Multisensor';
 
 // We are using bootstrap as the UI library
 import 'bootstrap/dist/css/bootstrap.css';
@@ -28,103 +24,33 @@ class App extends Component {
     this.state = {
       // Widgets that are available in the dashboard
       widgets: {
-        EngineTelemetricsWidget: {
-          type: BarChart,
-          title: 'Engine',
-        },
-        PerformanceWidget: {
-          type: DoughnutChart,
-          title: 'Reactor Temp',
-        },
-        ShipVitalTelemetricsWidget: {
-          type: LineChart,
-          title: 'Reactor Telemetrics',
-        },
+        MultisensorWidget: {
+          type: Multisensor,
+          title: 'Multisensor',
+        }
       },
       // Layout of the dashboard
       layout: {
         rows: [{
           columns: [{
-            className: 'col-md-12 col-sm-12 col-xs-12',
-            widgets: [{key: 'ShipVitalTelemetricsWidget'}],
-          }],
-        }, {
-          columns: [{
-            className: 'col-md-8 col-sm-8 col-xs-8',
-            widgets: [{key: 'EngineTelemetricsWidget'}],
-          }, {
-            className: 'col-md-4 col-sm-4 col-xs-4',
-            widgets: [{key: 'PerformanceWidget'}],
+            className: 'col-12',
+            widgets: [{key: 'MultisensorWidget'}],
           }],
         }],
       },
-      editMode: false,
-      isModalOpen: false,
-      addWidgetOptions: null,
     };
-  }
-
-  /**
-   * When a widget is removed, the layout should be set again.
-   */
-  onRemove = (layout) => {
-    this.setState({
-      layout: layout,
-    });
-  }
-
-  /**
-   * Adds new widgget.
-   */
-  onAdd = (layout, rowIndex, columnIndex) => {
-    // Open the AddWidget dialog by seting the 'isModalOpen' to true.
-    // Also preserve the details such as the layout, rowIndex, and columnIndex  in 'addWidgetOptions'.
-    //  This will be used later when user picks a widget to add.
-    this.setState({
-      isModalOpen: true,
-      addWidgetOptions: {
-        layout,
-        rowIndex,
-        columnIndex,
-      },
-    });
-  }
-
-  /**
-   * When a widget moved, this will be called. Layout should be given back.
-   */
-  onMove = (layout) => {
-    this.setState({
-      layout: layout,
-    });
-  }
-
-  /**
-   * This will be called when user tries to close the modal dialog.
-   */
-  onRequestClose = () => {
-    this.setState({
-      isModalOpen: false,
-    });
   }
 
   render() {
     return (
       <Container>
-        <AddWidgetDialog widgets={this.state.widgets} isModalOpen={this.state.isModalOpen} onRequestClose={this.onRequestClose} onWidgetSelect={this.handleWidgetSelection}/>
         <Header />
-        <EditBar onEdit={this.toggleEdit} />
         <Dashboard
           frameComponent={CustomFrame}
-          onRemove={this.onRemove}
           layout={this.state.layout}
           widgets={this.state.widgets}
-          editable={this.state.editMode}
-          onAdd={this.onAdd}
-          onMove={this.onMove}
-          addWidgetComponentText="Add New Widget"
+          editable={false}
         />
-
     </Container>
     );
   }
