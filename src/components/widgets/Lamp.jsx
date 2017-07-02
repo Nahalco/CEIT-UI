f158d277f3db9b598cdd4b65ec0cc61d37c06d6e
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 03:01:04 +0430

    Lamp widget was created.

---
+++ src/components/widgets/Lamp.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+class Lamp extends React.Component {
+  constructor (props) {
+    super(props)
+    this.things = props.things
+    this.state = {
+      selectedThing: this.things[0]
+    }
+  }
+
+  turn (on) {
+    this.state.selectedThing.configuration = {
+      on
+    }
+  }
+
+  changeSelectedThing (thingId) {
+    let selectedThing = null
+    for (const thing of this.things) {
+      if (thing.id === thingId) {
+        selectedThing = thing
+      }
+    }
+    this.setState({
+      selectedThing
+    })
+  }
+
+  render () {
+    const options = []
+    for (const thing of this.things) {
+      options.push((
+        <option value={thing.id} key={thing.id} >{thing.id}</option>
+      ))
+    }
+    return (
+      <div>
+        <h4>Lamp
+          <select className='custom-select mb-2 ml-sm-2 mb-sm-0' onChange={(event) => this.changeSelectedThing(event.target.value)}>
+            {options}
+          </select>
+        </h4>
+        <div className='btn-group' data-toggle='buttons'>
+          <label className='btn btn-lg btn-info'>
+            <input type='checkbox' autoComplete='off' onClick={() => this.turn(true)} /> On
+          </label>
+          <label className='btn btn-lg btn-danger'>
+            <input type='checkbox' autoComplete='off' onClick={() => this.turn(false)} /> Off
+          </label>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Lamp
