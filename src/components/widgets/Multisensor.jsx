9927888c7121841a5228ee75cf1cb94ce78c6933
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 03:22:02 +0430

    Multisensor colors were improved.

--- src/components/widgets/Multisensor.jsx
+++ src/components/widgets/Multisensor.jsx
@@ -45,7 +45,12 @@ class Multisensor extends React.Component {
           value={this.state.temperature}
           minValue={0}
           maxValue={50}
-          majorTicks={['0', '5', '15', '20', '25', '30', '35', '40', '45', '50']}
+          majorTicks={['10', '15', '20', '25', '30', '35', '40', '45', '50']}
+          highlights={[
+            { from: 0, to: 25, color: 'rgba(0,255,0,.15)' },
+            { from: 25, to: 35, color: 'rgba(255,255,0,.15)' },
+            { from: 35, to: 50, color: 'rgba(255,30,0,.25)' }
+          ]}
           minorTicks={2} />
         <RadialGauge
           units='%'
@@ -55,7 +60,7 @@ class Multisensor extends React.Component {
           minValue={0}
           maxValue={100}
           majorTicks={['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']}
-          minorTicks={5} />
+          minorTicks={6} />
         <RadialGauge
           units='%'
           title='Light'
@@ -64,7 +69,12 @@ class Multisensor extends React.Component {
           minValue={0}
           maxValue={100}
           majorTicks={['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']}
-          minorTicks={5} />
+          highlights={[
+            { from: 0, to: 30, color: 'rgba(0,0,0,.15)' },
+            { from: 50, to: 80, color: 'rgba(255,255,0,.15)' },
+            { from: 80, to: 100, color: 'rgba(255,255,0,.5)' }
+          ]}
+          minorTicks={0} />
       </div>
     )
   }
