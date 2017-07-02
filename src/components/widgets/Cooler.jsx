7010626558d2a803a7a704aa0ba22eb6a3a631e5
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 04:15:48 +0430

    Cooler widget was added.

---
+++ src/components/widgets/Cooler.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import {I1820Thing} from '@i1820/api'
+
+class Cooler extends React.Component {
+  constructor (props) {
+    super(props)
+    this.thing = props.thing
+    this.state = {
+      mode: 'off'
+    }
+  }
+
+  turn (event) {
+    switch (event.target.value) {
+      case '0':
+        this.setState({
+          mode: 'off'
+        })
+        this.thing.configuration = {
+          on: false
+        }
+        break
+      case '10':
+        this.setState({
+          mode: 'Cool'
+        })
+        this.thing.configuration = {
+          on: true,
+          temperature: 16
+        }
+        break
+      case '20':
+        this.setState({
+          mode: 'Fan'
+        })
+        this.thing.configuration = {
+          on: true,
+          temperature: 25
+        }
+        break
+      case '30':
+        this.setState({
+          mode: 'Heat'
+        })
+        this.thing.configuration = {
+          on: true,
+          temperature: 30
+        }
+        break
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <p>{this.state.mode}</p>
+        <input type='range' defaultValue='0' min='0' max='30' step='10' onChange={(event) => this.turn(event)} />
+      </div>
+    )
+  }
+}
+
+Cooler.propTypes = {
+  thing: React.PropTypes.instanceOf(I1820Thing)
+}
+
+export default Cooler
