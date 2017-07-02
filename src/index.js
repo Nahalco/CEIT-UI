4d55a95286e46d10d06eeb7f38a68144abf2f661
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 00:31:46 +0430

    Javascritp entry point was added.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 03-07-2017
+ * |
+ * | File Name:     index.js
+ * +===============================================
+ */
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Dashboard from './components/Dashboard';
+
+ReactDOM.render(<Dashboard />,  document.getElementById('root'));
