710e613eb10c8032a57cc29153827738671aa772
Parham Alvani <parham.alvani@gmail.com>
2017-07-04 10:58:32 +0430

    Fire alarm component was added.

---
+++ src/components/widgets/FireAlarm.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import {I1820Thing} from '@i1820/api'
+
+class FireAlarm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.thing = props.thing
+
+    this.state = {
+      alram: false
+    }
+
+    this.thing.on('log', (result) => {
+      this.setState({
+        gas: result['gas']
+      })
+    })
+  }
+
+  componentDidMount () {
+  }
+
+  render () {
+    if (this.state.alarm) {
+      return (
+        <div className='alert alert-danger' role='alert'>
+          <h4 className='alert-heading'>Fire !?!</h4>
+        </div>
+      )
+    } else {
+      return (
+        <div className='alert alert-success' role='alert'>
+          <h4 className='alert-heading'>There is no fire</h4>
+        </div>
+      )
+    }
+  }
+}
+
+FireAlarm.propTypes = {
+  thing: React.PropTypes.instanceOf(I1820Thing)
+}
+
+export default FireAlarm
