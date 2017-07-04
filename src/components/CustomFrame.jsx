c805bae4af76f04ddcf7206002d8e440715f7a8c
Parham Alvani <parham.alvani@gmail.com>
2017-07-04 23:07:10 +0430

    Custom frame issuws were corrected.

--- src/components/CustomFrame.jsx
+++ src/components/CustomFrame.jsx
@@ -1,28 +1,24 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes } from 'react'
 
 const CustomFrame = ({children, onRemove, editable, title}) => {
   return (
-    <div className="x_panel fixed_height_320">
-      <div className="x_title">
-          <h2>{title}</h2>
-          <ul className="nav navbar-right panel_toolbox">
-              {editable && <li><a onClick={() => {onRemove();}} className="close-link"><i className="fa fa-close"></i></a>
-              </li>}
-          </ul>
-          <div className="clearfix"></div>
+    <div className='x_panel fixed_height_320'>
+      <div className='x_title'>
+        <h2>{title}</h2>
+        <div className='clearfix' />
       </div>
-      <div className="x_content">
+      <div className='x_content'>
         {children}
       </div>
-  </div>
-);
-};
+    </div>
+  )
+}
 
 CustomFrame.propTypes = {
   children: PropTypes.element,
   onRemove: PropTypes.func,
   editable: PropTypes.bool,
-  title: PropTypes.string,
-};
+  title: PropTypes.string
+}
 
-export default CustomFrame;
+export default CustomFrame
