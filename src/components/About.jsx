175b8b7dc3811bb1fda228db1935aa8634cf5633
Parham Alvani <parham.alvani@gmail.com>
2017-07-07 14:58:14 +0430

    About page was added.

---
+++ src/components/About.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import Header from './Header'
+import Sidebar from 'react-sidebar'
+
+// We are using bootstrap as the UI library
+import 'bootstrap/dist/css/bootstrap.css'
+
+// styles
+import '../styles/main.less'
+import '../styles/nav.less'
+import '../styles/frame.less'
+
+class App extends React.Component {
+  render () {
+    return (
+      <Sidebar
+        styles={{
+          sidebar: {
+            background: 'rgba(0,0,0,0.6)'
+          }
+        }}
+        sidebar={
+          <ul className='nav nav-pills flex-column'>
+            <li className='nav-item'>
+              <a className='nav-link text-white bg-warning' href='/'>Home</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link text-warning' href='#'>Alarms</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link text-warning' href='#'>About</a>
+            </li>
+          </ul>
+        }
+        docked
+      >
+        <Header />
+        <div>
+          We are the Aolab
+        </div>
+      </Sidebar>
+    )
+  }
+}
+
+export default App
