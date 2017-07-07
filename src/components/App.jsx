54b43f8ce52ecd9ba3361c412efc3fc8765352cf
Parham Alvani <parham.alvani@gmail.com>
2017-07-07 09:45:47 +0430

    Sidebar was added.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Header from './Header'
 import Dashboard from './Dashboard'
+import Sidebar from 'react-sidebar'
 
 // We are using bootstrap as the UI library
 import 'bootstrap/dist/css/bootstrap.css'
@@ -14,10 +15,30 @@ import '../styles/frame.less'
 class App extends React.Component {
   render () {
     return (
-      <div>
+      <Sidebar
+        styles={{
+          sidebar: {
+            background: 'rgba(0,0,0,0.6)'
+          }
+        }}
+        sidebar={
+          <ul className='nav nav-pills flex-column'>
+            <li className='nav-item'>
+              <a className='nav-link text-white bg-warning' href='#'>Home</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link text-warning' href='#'>Alarms</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link text-warning' href='#'>About</a>
+            </li>
+          </ul>
+        }
+        docked
+      >
         <Header />
         <Dashboard />
-      </div>
+      </Sidebar>
     )
   }
 }
