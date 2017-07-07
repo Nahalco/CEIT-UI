e8e49a4b75b693ebaff968f12be699e981a9a7fc
Parham Alvani <parham.alvani@gmail.com>
2017-07-07 14:58:22 +0430

    Router was setup.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@
  */
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import {HashRouter, Route} from 'react-router-dom'
 import App from './components/App'
+import About from './components/About'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render((
+  <HashRouter>
+    <Route path='/' component={App} />
+    <Route path='/about' component={About} />
+  </HashRouter>
+), document.getElementById('root'))
